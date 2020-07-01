20ef0f2be3a5ca602482eda0bd8e888445012af6
nicolas998 <nicolas.velasquezgiron@gmail.com>
2020-07-01 00:26:46 -0500

    Develop of the event based kge figures and yearly based figures

--- aclayer/_nbdev.py
+++ aclayer/_nbdev.py
@@ -3,7 +3,12 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"get_intersec": "00_core.ipynb",
-         "percentiles": "00_core.ipynb"}
+         "percentiles": "00_core.ipynb",
+         "plot_hist2d": "00_core.ipynb",
+         "get_histogram": "00_core.ipynb",
+         "summary_metric": "00_core.ipynb",
+         "get_intersec_events": "00_core.ipynb",
+         "colors": "00_core.ipynb"}
 
 modules = ["core.py"]
 
